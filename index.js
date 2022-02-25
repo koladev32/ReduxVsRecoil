53c8b4db7fd4ac351295490185a082bdd8a9ae86
koladev <onaelmangabo@gmail.com>
2022-02-26 00:08:31 +0100

    - feat: register store to app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import store from './store';
 
-AppRegistry.registerComponent(appName, () => App);
+const ReduxApp = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
