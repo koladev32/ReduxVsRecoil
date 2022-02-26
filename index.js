c11f71e4bafca5dd3741444615e2dd94e84c9478
koladev <onaelmangabo@gmail.com>
2022-02-26 01:12:59 +0100

    - register app in  recoilroot

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
-import store from './store';
+import {RecoilRoot} from 'recoil';
 
-const ReduxApp = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
+const RecoilApp = () => (
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
 
-AppRegistry.registerComponent(appName, () => ReduxApp);
+AppRegistry.registerComponent(appName, () => RecoilApp);
